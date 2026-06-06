9b44ba479bd171e3a1d0a0c9151a844d221af92f
saiharshini49 <saiharshinipayideti@gmail.com>
2026-06-06 12:05:43 +0530

    Add files via upload

---
+++ contactbook/contactbook.py
@@ -0,0 +1,50 @@
+contacts = {}
+
+while True:
+    print("\n1.Add Contact")
+    print("2.View Contacts")
+    print("3.Search Contact")
+    print("4.Delete Contact")
+    print("5.Exit")
+
+    choice = input("Enter choice: ")
+
+    # Add Contact
+    if choice == "1":
+        name = input("Enter name: ")
+        phone = input("Enter phone number: ")
+        contacts[name] = phone
+        print("Contact Added!")
+
+    # View Contacts
+    elif choice == "2":
+        print("\nContact List:")
+        for name, phone in contacts.items():
+            print(name, "-", phone)
+
+    # Search Contact
+    elif choice == "3":
+        name = input("Enter name to search: ")
+
+        if name in contacts:
+            print(name, "-", contacts[name])
+        else:
+            print("Contact not found!")
+
+    # Delete Contact
+    elif choice == "4":
+        name = input("Enter name to delete: ")
+
+        if name in contacts:
+            del contacts[name]
+            print("Contact Deleted!")
+        else:
+            print("Contact not found!")
+
+    # Exit
+    elif choice == "5":
+        print("Thank You!")
+        break
+
+    else:
+        print("Invalid choice!")
